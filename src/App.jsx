597327fc0437063b813f68735d43d58af58e85d1
Joaquin Clausen <jlclausen99@gmail.com>
2023-03-04 14:58:17 -0300

    instalación de material icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <>
-      
+      <Navbar/>
     </>
   )
-  
+
 }
 
 export default App
