cda90d719b918e0a6ab8d2c8a6abe19e7dd8435d
Joaquin Clausen <jlclausen99@gmail.com>
2023-03-19 19:59:00 -0300

    detalle final parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {Navigate, Route, Routes, useParams } from 'react-router-dom';
 import Home from './components/Home';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import error404img from './components/img/404.jpg';
 import DetalleProducto from './components/DetalleProducto';
 
 function App() {
@@ -24,7 +23,7 @@ function App() {
         <Route path="/home" element={<Home text="BIENVENIDOS A GVG"/>}/>
         <Route path="/productos" element={<ItemListContainer/>}/>
         <Route path="/productos/:id" element={<DetalleProducto/>}/>
-        <Route path="*" element={<img className={styles.notFound}  src={error404img} alt="404 not found"/>}/>
+        <Route path="*" element={<img className={styles.notFound}  src="https://ucarecdn.com/8d79cbc5-bbb0-4664-9379-1e891283c778/404.jpg" alt="404 not found"/>}/>
       </Routes>
     </ThemeProvider>
   )
