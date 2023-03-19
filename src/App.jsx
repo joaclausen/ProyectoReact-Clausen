bbdf4584a13068823f289160727152e9b1bb4d6f
Joaquin Clausen <jlclausen99@gmail.com>
2023-03-19 03:53:53 -0300

    agregado routes y una api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
 import styles from './app.module.css'
 import Navbar from './components/Navbar'
 import ItemListContainer from './components/ItemListContainer'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '43042d8fc4msha20aff58a70e139p1d9673jsn2947d1830db7',
+      'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+    }
+  };
+
+  const [productos, setProductos] = useState([]);
+  const [count, setCount] = useState(1);
+
+  const getJuegos = async () => {
+    try {
+      const result = await fetch(`https://rawg-video-games-database.p.rapidapi.com/games?key=b60fdf46e0c349e4b45580f1254d9014&page=${count}`, options);
+      const data = await result.json();
+      setProductos(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const siguiente = () =>{
+    setCount(count + 1);
+ 
+  }
+  const anterior = () =>{
+    count>1? setCount(count - 1):setCount(1);
+    
+  }
+
+  useEffect(() => {
+    getJuegos();
+
+  }, [])
+  
+
 
   return (
     <nav className={styles.container}>
       <Navbar/>
-      <ItemListContainer text="BIENVENIDOS A OVG"/>
+      <ItemListContainer text="BIENVENIDOS A OVG" productos={productos}/>
+      <h2>count: {count}</h2>
+      <button onClick={siguiente}>+</button>
+      <button onClick={anterior}>-</button>
+      
     </nav>
   )
 
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,14 @@
 import style from "./itemlistcontainer.module.css"
 
-const ItemListContainer = ({text}) => {
+const ItemListContainer = ({text, productos}) => {
   return (
     <>
         <h1 className={style.titulo}>{text}</h1>
+        {productos.map((product)=>(
+
+          <p key={product.id}>{product.name}</p>
+
+        ))}
     </>
   )
 }
