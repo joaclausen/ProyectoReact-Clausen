f3d570b37207b804bcb2b3cfe2e696a3874db453
Joaquin Clausen <jlclausen99@gmail.com>
2023-04-12 20:50:55 -0300

    home finalizado

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./home.module.css";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 
 const Home = ({text}) => {
   return (
     <div>
         <h1 className={styles.titulo}>{text}</h1>  
         <p className={styles.buscar}>Search for your favorite video game</p>
-        <Button className={styles.boton} variant="contained" centered>
-          <Link className={styles.empezar} to="/productos">Start now!</Link>
-        </Button>
-       
-
+        <Box textAlign='center'>
+          <Button className={styles.boton} variant="contained" centered>
+            <Link className={styles.empezar} to="/productos">Start now!</Link>
+          </Button>
+        </Box>
     </div>
 
   )
