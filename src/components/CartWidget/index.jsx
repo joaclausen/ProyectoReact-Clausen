5fe2133fb30ebfbbde271d53f58bd5f32542a25a
Joaquin Clausen <jlclausen99@gmail.com>
2023-04-13 21:08:35 -0300

    cambios cart

--- src/components/CartWidget/index.jsx
+++ src/components/CartWidget/index.jsx
@@ -1,12 +1,13 @@
 import { Button } from "@mui/material"
 import styles from "./cartwidget.module.css"
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const CartWidget = ({carrito}) => {
   return (
     <div>
       {(carrito || []).map((cart)=>(
         <div>
-          <p key={carrito.titleId}>{cart.title} x{cart.cantidad}</p>
+          <p key={carrito.titleId}>{cart.title} x{cart.cantidad} <DeleteIcon/></p>
         </div>
       ))}
       <Button className={styles.checkout} variant="contained">Checkout</Button>
